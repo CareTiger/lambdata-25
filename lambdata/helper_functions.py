1f562084e379ce8e4a7f44f7dc56e8fb69ab7d8a
CareTiger <currency@gmail.com>
2021-04-05 13:57:40 -0500

    added train/test split function

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -5,8 +5,5 @@ def null_count(df):
 
 def train_test_split(df, frac):
     df_length = len(df)
-    return df_length, frac
-
-    # df = utils.shuffle(df)
-    # df1 = df[0:3]
-    # df2 = df[3:6]
+    cutoff = round(frac * df_length)
+    return df[:cutoff], df[cutoff:]
